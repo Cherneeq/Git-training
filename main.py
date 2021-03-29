9e64fe8d2b1ce8c429ba49188ac667e10add17d0
Cherneeq <jiaplija@gmail.com>
2021-03-29 16:59:34 +0700

    seven commit

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-a = int(input())
-b = int(input())
-operator = input()
-if operator == '-':
-    print(a - b)
-elif operator == '+':
-    print(a + b)
-elif operator == '*':
-    print(a * b)
-elif operator != '-' and operator != '+' and operator != '*' and operator != '/':
-     print('Операция невозможна')
-if operator == '/':
-   if b > 0:
-    print(a / b)
-   else:
-        print('На ноль делить нельзя')
+color_1 = input()
+color_2 = input()
+if color_1 == 'красный' and color_2 == 'красный':
+    print('красный')
+elif color_1 == 'красный' and color_2 == 'желтый':
+    print('оранжевый')
+elif color_1 == 'красный' and color_2 == 'синий':
+    print('фиолетовый')
+elif color_1 == 'желтый' and color_2 == 'желтый':
+    print('желтый')
+elif color_1 == 'желтый' and color_2 == 'красный':
+    print('оранжевый')
+elif color_1 == 'желтый' and color_2 == 'синий':
+    print('зеленый')
+elif color_1 == 'синий' and color_2 == 'синий':
+    print('синий')
+elif color_1 == 'синий' and color_2 == 'желтый':
+    print('зеленый')
+elif color_1 == 'синий' and color_2 == 'красный':
+    print('фиолетовый')
+else:
+    print('ошибка цвета')
+
