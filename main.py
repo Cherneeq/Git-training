712da24d80551cc12d7bde15edbe8337cd486df5
Cherneeq <s.chernyavskee@gmail.com>
2021-09-02 00:19:49 +0700

    it's the tasks

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 
-
-
-
-# num = int(input())
-# m = 0
-# while num > 0:
-#     if(num % 10 > m):
-#         m = num % 10
-#     num //= 10
-# print(m)
+for b in range(1, 101):
+    for c in range(1, 101):
+        for t in range(1, 100):
+            if (10 * b + 5 * c + 0.5 * t == 100) and (b + c + t == 100):
+                print(b, c, t)
