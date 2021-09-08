6e73be78bc42ea2afcd000dd0417a278eeb1ca61
Cherneeq <s.chernyavskee@gmail.com>
2021-09-09 00:01:51 +0700

    Fierce task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-
-for b in range(1, 101):
-    for c in range(1, 101):
-        for t in range(1, 100):
-            if (10 * b + 5 * c + 0.5 * t == 100) and (b + c + t == 100):
-                print(b, c, t)
+for a in range(1, 33):
+    for b in range(1, 33):
+        for c in range(1, 33):
+            for d in range(1, 33):
+                if a ** 3 + b ** 3 == c ** 3 + d ** 3 and a != b and a != c and a != d and b != c and b != d and c != d:
+                    print(a ** 3 + b ** 3)
